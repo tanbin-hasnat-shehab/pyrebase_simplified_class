261e917a8be636d5307223268c29cd5b8a125f44
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2022-05-14 14:19:02 +0600

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,38 +1,44 @@
 #from pyrebase_module import pyrebase_2D_database
 from pyrebase_module import*
 
-db=object_database('saif')
-'''
-db.input_data(pos='Name',value='shehab')
-db.input_data(pos='place',value='bndrbn')
-'''
+db=object_database(database_name='mydb')
+
+#db.input_data(path='ssgmailcom/all data/Name',value='hasnat')
+#db.input_data(path='shehabgmailcom/all data',attribute='place',value='khag')
 
 
 
 
 
-#db.delete_data(key='place')
+#db.delete_data(data_path='saifgmailcom/all data/place')
 
-'''
-dic,arr=db.show_data()
-print(dic)
-'''
+
+#tup=db.show_data()
+
+#pp.pprint(tup)
+
 
 #######################################################
 
 '''
-sdb=storage_database('shehab')
-sdb.upload_file(file_attribute='passwords',file_name='creds.json')
+sdb=storage_database(database_name='mdb')
+sdb.upload_file(path='hasnat',file_attribute='pic',file_name='profile_picture.png')
+sdb.upload_file(path='hasnat',file_attribute='profile',file_name='a.pdf')
+sdb.upload_file(path='hasnat',file_attribute='map',file_name='map.jpg')
+'''
+
+
+
+'''
+sdb=storage_database(database_name='mdb')
+sdb.delete_file(file_attribute='code',path='old')
 '''
 
 
 '''
-sdb=storage_database('saif')
-sdb.delete_file(file_attribute='pic')
+sdb=storage_database(database_name='mdb')
+#sdb.download_file(path='hasnat')
+#sdb.download_file(path='new')
+sdb.download_file(path='old',file_attribute='pic')
 '''
-
-
-sdb=storage_database('shehab')
-sdb.download_file()
-
 
